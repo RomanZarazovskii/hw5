60d51f94e154466cd11810b9b83cf8735b9d29bb
RomanZarazovskii <124078828+RomanZarazovskii@users.noreply.github.com>
2024-08-14 19:49:19 +0000

    Deploying to gh-pages from @ RomanZarazovskii/hw5@1008ea44a1a7b7048b78e0a4748a4f448170050e 🚀

---
+++ index.00ab7037.js
@@ -0,0 +1,2 @@
+document.getElementById("btn"),document.getElementById("key");
+//# sourceMappingURL=index.00ab7037.js.map
---
+++ index.5a1fa044.js
@@ -0,0 +1,2 @@
+document.getElementById("btn"),document.getElementById("key");
+//# sourceMappingURL=index.5a1fa044.js.map
